9c94e8b71fa3f53a5a7dc852a009932d68b598ae
yadaiio <y.clintjens@gmail.com>
2025-01-28 19:25:13 +0100

    moYaUtility Kommentare hinzufügen

--- src/moYaUtility.java
+++ src/moYaUtility.java
@@ -1,8 +1,17 @@
+/*Diese Klasse dient als Ansammlung verschiedener Funktionen, die wir
+ * für das Projekt benötigen. MOYA steht für Moritz und Yada, und utility
+ * steht für "Werkzeug". Diese Klasse ist also unser Werkzeugkoffer.
+ */
+
 public class moYaUtility {
-    public static void chageCursor(int row, int column){
+
+    //Funktion namens changeCursor, wo wir jeweils zwei Integer reingeben
+    public static void changeCursor(int row, int column){
+
+        //binärer Code für die Escape-Sequenz, ASCII 27 oder ESC, nicht sichtbar
         char escCode = 0x1B;
-        
-        System.out.print(String.format("%c[%d;%df",escCode,row,column));
-        System.out.println("Hallo, Welt!");
+
+        //c% fügt einen variablen Char ein, d fügt Digit ein, f beendet die escape-Sequenz
+        System.out.print(String.format("%c[%d;%df",escCode,row,column)); 
     }
 }
